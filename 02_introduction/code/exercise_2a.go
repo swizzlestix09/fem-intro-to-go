28aa17d734f81c2c9df835bfdfd61eb3902fbf36
swizzlestix09 <angely.rodriguez@gmail.com>
2021-12-22 15:07:29 -0500

    exercise 2 complete

---
+++ 02_introduction/code/exercise_2a.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World.")
+	fmt.Printf("Hi! My name is Angie. I have lived in da Bronx for 9 years. They say the weather is amazing, which is kinda true.")
+}
