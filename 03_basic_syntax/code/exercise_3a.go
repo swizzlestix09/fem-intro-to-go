200f520884b821ef2e506cd52ee38fc4bf85443d
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-04 17:53:48 -0500

    exercise 3a completew notes

--- 03_basic_syntax/code/exercise_3a.go
+++ 03_basic_syntax/code/exercise_3a.go
@@ -9,4 +9,9 @@ func main() {
 			fmt.Println(string(letter))
 		}
 	}
+	//if you are not going to need a variable, in this case index, you can use the _ to notify go that this will not be needed.
+	for _, letter := range sentance {
+		fmt.Println(string(letter))
+
+	}
 }
