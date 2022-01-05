90c7f64a3e5bf17eff04d5695a3c9e985249dc81
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-05 16:35:28 -0500

    function ex. 4a complete

--- 04_complex_structures/code/functions.go
+++ 04_complex_structures/code/functions.go
@@ -22,12 +22,22 @@ func printAge(age1, age2 int) (ageOfSally, ageOfBob int) {
 	//notice that since these variables
 	ageOfSally = age1
 	ageOfBob = age2
+	//will return implicitly without having to re write variables
 	return
 }
 
+//if you're unsure of how many arguments your function will recieve there is a
+//spread operator available - bear in mind that since multiple items are being returned, this is now a collection - in order to access items passed one must use a for loop (_ used since index is not needed):
+func printAges(ages ...int) {
+	for _, value := range ages {
+		fmt.Println(value)
+	}
+	return
+}
 func main() {
 	checkAge(26)
-	// 	x, y := printAge(10, 21)
-	// 	fmt.Println(x)
-	// 	fmt.Println(y)
+	printAges(14, 17, 43)
+	x, y := printAge(10, 21)
+	fmt.Println(x)
+	fmt.Println(y)
 }
