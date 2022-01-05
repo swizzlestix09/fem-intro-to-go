6e4d0e1ce4b810bb665651660f8c4037d3b2fec9
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-05 16:04:54 -0500

    functions

--- 04_complex_structures/code/functions.go
+++ 04_complex_structures/code/functions.go
@@ -1,14 +1,32 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// func printAge(age1, age2 int) (ageOfSally, ageOfBob int) {
-// 	ageOfSally = age1
-// 	ageOfBob = age2
-// 	return
+//if returning one thing, can use this syntax:
+
+// func checkAge(age int) int {
+// 	fmt.Println(age)
+// 	return age
 // }
 
+//if returning multple things, can announce multiple types i.e:
+
+func checkAge(age int) (int, int) {
+	fmt.Println(age)
+	return 0, age
+}
+
+//can also name return values:
+
+func printAge(age1, age2 int) (ageOfSally, ageOfBob int) {
+	//notice that since these variables
+	ageOfSally = age1
+	ageOfBob = age2
+	return
+}
+
 func main() {
+	checkAge(26)
 	// 	x, y := printAge(10, 21)
 	// 	fmt.Println(x)
 	// 	fmt.Println(y)
