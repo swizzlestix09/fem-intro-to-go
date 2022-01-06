2654ebd6375a5d6d1e4a09af29adb6dda896f710
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-06 16:45:24 -0500

    exercise 4 complete

--- 04_complex_structures/code/exercise_4c.go
+++ 04_complex_structures/code/exercise_4c.go
@@ -10,14 +10,33 @@ func average(grades [5]float64) float64 {
 	return total / float64(len(grades))
 }
 
+var pets = map[string]string{
+	"aimee": "cat",
+	"yago":  "cat",
+	"puppy": "cat",
+	"betty": "dog",
+}
+
+func petValidation(pet string) bool {
+	_, isValid := pets[pet]
+	return isValid
+}
+
+var groceries = []string{"corn", "cookies", "salad", "onions", "sirloin", "grapes", "weed"}
+
+func addToGroceries(foods ...string) []string {
+	listOfFoods := groceries
+	for _, food := range foods {
+		listOfFoods = append(groceries, food)
+	}
+	return listOfFoods
+
+}
+
 func main() {
 	scores := [5]float64{87, 79, 88, 91, 69}
-	pets := map[string]string{
-		"aimee": "cat",
-		"yago":  "cat",
-		"puppy": "cat",
-		"betty": "dog",
-	}
 
 	fmt.Println(average(scores))
+	fmt.Println(petValidation("aimee"))
+	fmt.Println(addToGroceries("Kiwi", "Pinapples", "mamahuevos"))
 }
