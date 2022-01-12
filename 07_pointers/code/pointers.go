a144d65122c980a0213286265fcf007bf6d80797
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-12 11:51:39 -0500

    working on pointers

--- 07_pointers/code/pointers.go
+++ 07_pointers/code/pointers.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // func main() {
 // 	var name string
 // 	var namePointer *string
@@ -11,16 +13,16 @@ package main
 
 // // ******************************************************
 
-// func main() {
-// 	var name string = "Beyonce"
-// 	var namePointer *string = &name
-// 	var nameValue = *namePointer
+func main() {
+	var name string = "Beyonce"     //name is set to Beyonce
+	var namePointer *string = &name //namePointer is pointing to address in memory
+	var nameValue = *namePointer    //* is defererencing namePointer to what's held there, which will print Beyonce
 
-// 	fmt.Println("Name:", name)
-// 	fmt.Println("Name *:", namePointer)
-// 	fmt.Println("Name Value:", nameValue)
+	fmt.Println("Name:", name)
+	fmt.Println("Name *:", namePointer)
+	fmt.Println("Name Value:", nameValue)
 
-// }
+}
 
 // // ******************************************************
 
@@ -36,13 +38,21 @@ package main
 
 // // ******************************************************
 
-//Coordinates represent a latitude and longitiude
-type Coordinates struct {
-	X, Y float64
-}
+// //Coordinates represent a latitude and longitiude
+// type Coordinates struct {
+// 	X, Y float64
+// }
 
-var c = Coordinates{X: 10, Y: 20}
+// var c = Coordinates{X: 10, Y: 20}
 
-func main() {
-  coordinatedMemAdd :=
-}
+// func main() {
+// 	coordinatedMemAdd := &c //address location of var c which holds coordinates
+// 	coordinatedMemAdd.X = 200
+// 	coordinatedMemAdd.Y = 400
+// 	fmt.Println(coordinatedMemAdd, c) //both will be changed since the memory address was associated with the change
+// 	//there is a shortcut for structs with go were just referencing the struct will change it, like below
+// 	newCoordinates := c
+// 	newCoordinates.X = 80.12
+// 	newCoordinates.Y = 1123.3212
+// 	fmt.Println(newCoordinates) //coordinated in c has been changed.
+// }
