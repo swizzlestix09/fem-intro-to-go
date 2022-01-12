bd1d3236342e0dcb2efabcb65eb5cbd0b2108dc9
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-12 15:52:32 -0500

    implemented a server, working on routes

--- 11_server/code/server.go
+++ 11_server/code/server.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+//http.ResponseWriter is a built-in type from http library
+//http.Request is another builtin type that is a pointer
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Home")
+}
+
+func todos(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("todos.html")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Template error", err)
+	}
+	err = t.Execute(w, nil)
+}
 
 func main() {
-	fmt.Println("Building a web server")
+	http.HandleFunc("/", home)
+	http.HandleFunc("/todos/", todos)
+	fmt.Println("Server is running on port: 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
