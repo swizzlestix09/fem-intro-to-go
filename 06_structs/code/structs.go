abca553efa139c9e29ac92faa82a8496405081bd
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-12 11:41:56 -0500

    working on pointers

--- 06_structs/code/structs.go
+++ 06_structs/code/structs.go
@@ -30,17 +30,23 @@ func describeUser(u User) string {
 }
 
 func describeGroup(g Group) string {
+	if len(g.Users) > 2 {
+		g.SpaceAvailable = false
+	}
+
 	desc := fmt.Sprintf("This user group has %b users. The newest user is %s %s. Space Available? %t", len(g.Users), g.NewestUser.FirstName, g.NewestUser.LastName, g.SpaceAvailable)
+
 	return desc
 }
 
 func main() {
 	u := User{ID: 1, FirstName: "Marilyn", LastName: "Monroe", Email: "marilyn.monroe@gmail.com"}
 	u2 := User{ID: 2, FirstName: "Jane", LastName: "Eyre", Email: "jedizzle@gmail.com"}
+	u3 := User{ID: 2, FirstName: "Audrey", LastName: "Hepburn", Email: "aHep01@gmail.com"}
 
 	g := Group{
 		Role:           "Admin",
-		Users:          []User{u, u2},
+		Users:          []User{u, u2, u3},
 		NewestUser:     u2,
 		SpaceAvailable: true,
 	}
@@ -49,4 +55,5 @@ func main() {
 	fmt.Println(u.FirstName) //like objects, can access individual properties with dot notation
 	fmt.Println(describeUser(u))
 	fmt.Println(describeGroup(g))
+	fmt.Println("After describeGroup call", g)
 }
--- 07_pointers/code/pointers.go
+++ 07_pointers/code/pointers.go
@@ -1,14 +1,13 @@
 package main
 
-import "fmt"
+// func main() {
+// 	var name string
+// 	var namePointer *string
 
-func main() {
-	var name string
-	var namePointer *string
-
-	fmt.Println("Name:", name)
-	fmt.Println("Name *:", namePointer)
-}
+// 	fmt.Println("Name:", name)
+// 	fmt.Println("Name *:", namePointer)
+// 	fmt.Println(&name)
+// }
 
 // // ******************************************************
 
@@ -25,14 +24,25 @@ func main() {
 
 // // ******************************************************
 
-// func changeName(n string) {
-// 	n = strings.ToUpper(n)
+// func changeName(n *string) {
+// 	*n = strings.ToUpper(*n) //the * takes the memory address and produces the value for modification
 // }
 
 // func main() {
 // 	name := "Elvis"
-// 	changeName(name)
+// 	changeName(&name) //want to send actual address where name is stored to change it properly
 // 	fmt.Println(name)
 // }
 
 // // ******************************************************
+
+//Coordinates represent a latitude and longitiude
+type Coordinates struct {
+	X, Y float64
+}
+
+var c = Coordinates{X: 10, Y: 20}
+
+func main() {
+  coordinatedMemAdd :=
+}
