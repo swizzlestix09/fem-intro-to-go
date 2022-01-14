97ba2bd4fbabaebdda61ce01de008c925a3b9e46
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-13 19:30:12 -0500

    Server hittign API complete

--- 11_server/code/server.go
+++ 11_server/code/server.go
@@ -9,9 +9,9 @@ import (
 
 //http.ResponseWriter is a built-in type from http library
 //http.Request is another builtin type that is a pointer
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Home")
-}
+// func home(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("Home")
+// }
 
 type Todo struct {
 	Title   string
@@ -40,14 +40,28 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 	err = t.Execute(w, pageVariables)
 }
 
-func addTodo(w http.ResponseWriter, r *http.Request) {
+func addTodos(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm() // parses through request
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Request parsing error: ", err)
+	}
+
+	todo := Todo{
+		Title:   r.FormValue("title"),
+		Content: r.FormValue("content"),
+	}
+
+	todos = append(todos, todo)
+	log.Print(todos)
+	http.Redirect(w, r, "/todos/", http.StatusSeeOther)
 
 }
 
 func main() {
-	http.HandleFunc("/", home)
+	//http.HandleFunc("/", home)
 	http.HandleFunc("/todos/", getTodos)
-	http.HandleFunc("/add-todos", addTodos)
+	http.HandleFunc("/add-todo/", addTodos)
 	fmt.Println("Server is running on port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- 12_api/code/api.go
+++ 12_api/code/api.go
@@ -1,12 +1,86 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 // BaseURL is the base endpoint for the star wars API
 const BaseURL = "https://swapi.dev/api/"
 
+//Planet is a planet type
+type Planet struct {
+	Name       string `json:"name"`
+	Terrain    string `json:"terrain"`
+	Population string `json:"population"`
+}
+
+//Person is a person type
+type Person struct {
+	Name         string `json:"name"`
+	HomeworldURL string `json:"homeworld"`
+	Homeworld    Planet
+}
+
+//Allpeople is a collection of Person types
+type AllPeople struct {
+	People []Person `json:"results"`
+}
+
+func (p *Person) getHomeworld() {
+	res, err := http.Get(p.HomeworldURL)
+
+	if err != nil {
+		log.Print("error fetching homeworld", err)
+	}
+	var bytes []byte
+	if bytes, err = ioutil.ReadAll(res.Body); err != nil {
+		log.Print("Error reading res.Body", err)
+	}
+
+	json.Unmarshal(bytes, &p.Homeworld)
+}
+
+//Route to get people fmt.fPrint that
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprint(w, "gettin people")
+	res, err := http.Get(BaseURL + "people")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("Failed to req star wars people")
+	}
+
+	fmt.Println(res)
+
+	bytes, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("failed to parse request body")
+	}
+
+	var people AllPeople
+
+	fmt.Println(string(bytes))
+	if err := json.Unmarshal(bytes, &people); err != nil {
+		fmt.Println("error parsing json", err)
+	}
+
+	fmt.Println(people)
+
+	for _, pers := range people.People {
+		pers.getHomeworld()
+		fmt.Println(pers)
+	}
+}
+
 func main() {
 	fmt.Println(BaseURL)
+	http.HandleFunc("/people", getPeople)
+	fmt.Println("Serving on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
