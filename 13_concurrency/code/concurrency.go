414e53dd34a8ba8c5e2f47e88dae3b8e71444050
swizzlestix09 <angely.rodriguez@gmail.com>
2022-01-13 19:43:00 -0500

    Concurrent Go

--- 13_concurrency/code/concurrency.go
+++ 13_concurrency/code/concurrency.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func say(s string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(s)
+var wg sync.WaitGroup
+
+func handlePanic() {
+	if r := recover(); r != nil {
+		fmt.Println("PANIC AT THE DISCO!")
+	}
+}
+
+func printStuff() {
+	defer wg.Done()
+	defer handlePanic()
+	for i := 1; i <= 3; i++ {
+		fmt.Println(i)
 		time.Sleep(time.Millisecond * 300)
 	}
 }
 
 func main() {
-	say("Hello")
-	say("There")
+	wg.Add(1)
+	go printStuff()
+	wg.Wait()
 }
+
+// func say(s string) {
+// 	for i := 0; i < 3; i++ {
+// 		fmt.Println(s)
+// 		time.Sleep(time.Millisecond * 300)
+// 	}
+// }
+
+// func main() {
+// 	go say("Hello")
+// 	say("There")
+// }
